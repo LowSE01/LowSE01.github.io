331c915b8814f004a6ec02251d014ccb206f0188
LowSE01 <12553995+LowSE01@users.noreply.github.com>
2023-04-02 07:23:06 +0900

    メンテナンスを楽にする。

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const {createApp} = Vue
 
+import Profile from './profile.js'
 import Skills from './skills.js'
+import Contact from './contact.js'
 
+createApp(Profile).mount('#profile')
 createApp(Skills).mount('#skills')
+createApp(Contact).mount('#contact')
---
+++ js/contact.js
@@ -0,0 +1,23 @@
+const contacts = [
+    {
+        "type": "twitter",
+        "icon_class": "fa-3x fa-brands fa-square-twitter",
+        "link": "https://twitter.com/VizFantasista",
+    },
+    {
+        "type": "facebook",
+        "icon_class": "fa-3x fa-brands fa-facebook",
+        "link": "https://www.facebook.com/seiichiro.murakami.35",
+    },
+    {
+        "type": "github",
+        "icon_class": "fa-3x fa-brands fa-github",
+        "link": "https://github.com/LowSE01",
+    },
+]
+
+export default {
+    data() {
+        return {"contacts": contacts}
+    },
+}
