c37c70b153364b4620aad5909b1c81d098e06297
LowSE01 <12553995+LowSE01@users.noreply.github.com>
2023-04-02 08:09:52 +0900

    events を追加。

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ const {createApp} = Vue
 
 import Profile from './profile.js'
 import Skills from './skills.js'
+import Works from './works.js'
+import Events from './events.js'
 import Contact from './contact.js'
 
 createApp(Profile).mount('#profile')
 createApp(Skills).mount('#skills')
+createApp(Works).mount('#works')
+createApp(Events).mount('#events')
 createApp(Contact).mount('#contact')
---
+++ js/events.js
@@ -0,0 +1,29 @@
+const events = [
+    {
+        "title": "Tableau✖️SQLユーザー会",
+        "text": "【第１回】TableauユーザーがSQLを学ぶ必要はあるのか？",
+        "tags": ["Tableau", "SQL"],
+        "link": "https://www.youtube.com/watch?v=wFSP32cFe8w",
+        "image": "https://i.ytimg.com/vi/wFSP32cFe8w/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD4Dql-OpQsmTbFapta0UmTrfc-Hw",
+    },
+    {
+        "title": "Tableau✖️SQLユーザー会",
+        "text": "【第４回】めくるめくJOINの世界へ!!レッツゴー!!",
+        "tags": ["Tableau", "SQL"],
+        "link": "https://www.youtube.com/watch?v=HyCj35LAq1o&t=3s",
+        "image": "https://i.ytimg.com/vi/HyCj35LAq1o/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCwG1bK0ScOkOYTsG6hKosNu5CFGg",
+    },
+    {
+        "title": "Tableau✖️SQLユーザー会",
+        "text": "【最終回】結局SQLは誰が学ぶべき？ #JTUGRadioNight ＃53",
+        "tags": ["登壇", "Tableau", "SQL"],
+        "link": "https://www.youtube.com/watch?v=Y2T5qj3UQLI&t=1s",
+        "image": "https://i.ytimg.com/vi/Y2T5qj3UQLI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDFv2_WzqFTgR_gGT9fMPNLwDviyA",
+    },
+]
+
+export default {
+    data() {
+        return {"events": events}
+    }
+}
