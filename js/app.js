e70b0f6bc046c255f363e0255f3f2ab488c39596
LowSE01 <12553995+LowSE01@users.noreply.github.com>
2023-04-03 00:39:53 +0900

    Worksを追加。

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ const {createApp} = Vue
 
 import Profile from './profile.js'
 import Skills from './skills.js'
-// import Works from './works.js'
+import Works from './works.js'
 import Events from './events.js'
 import Contact from './contact.js'
 
 createApp(Profile).mount('#profile')
 createApp(Skills).mount('#skills')
-// createApp(Works).mount('#works')
+createApp(Works).mount('#works')
 createApp(Events).mount('#events')
 createApp(Contact).mount('#contact')
---
+++ js/works.js
@@ -0,0 +1,22 @@
+const works = [
+    {
+        "title": "OSS Data Platform",
+        "text": "実務にて構築していたOSSによるデータ基盤の内容をもとに、データパイプラインなどを含んだ一連の構成をDockerで再現しています。",
+        "tags": ["データ基盤",],
+        "link": "https://github.com/LowSE01/oss-data-platform-document",
+        "image": "./image/AdobeStock_144803025.png",
+    },
+    {
+        "title": "Tableau Public",
+        "text": "Tableau Public個人ページ",
+        "tags": ["Tableau",],
+        "link": "https://public.tableau.com/app/profile/lowse01",
+        "image": "./image/TableauPublic.png",
+    },
+]
+
+export default {
+    data() {
+        return {"works": works}
+    }
+}
